7d61c4ab6b5447acc110b10cf4a307c8de5e1ca7
ansheng1021 <ansheng1021@163.com>
2020-05-19 11:02:41 +0800

    demo

---
+++ pages/index/index.js
@@ -0,0 +1,93 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    info:[
+      {time:'1:15:10',second:4510},
+      {time:'2:20:47',second:8447}
+    ],
+    inteval:''
+  },
+  //事件处理函数
+  bindViewTap: function() {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  formatSeconds(value) { 
+    var theTime = parseInt(value);// 需要转换的时间秒 
+    var theTime1 = 0;// 分 
+    var theTime2 = 0;// 小时 
+    var theTime3 = 0;// 天
+    var timeZero = ''
+    var time1Zero = ''
+
+    if(theTime > 60) { 
+     theTime1 = parseInt(theTime/60); 
+     theTime = parseInt(theTime%60); 
+     if(theTime1 > 60) { 
+      theTime2 = parseInt(theTime1/60); 
+      theTime1 = parseInt(theTime1%60); 
+      if(theTime2 > 24){
+       //大于24小时
+       theTime3 = parseInt(theTime2/24);
+       theTime2 = parseInt(theTime2%24);
+      }
+     } 
+    } 
+    var result = '';
+    if(theTime > 0){
+      if(theTime <10){
+        timeZero = 0
+      }
+     result = "" + timeZero +parseInt(theTime);
+    }else{
+      result = "00";
+    }
+    if(theTime1 > 0) { 
+      if(theTime1 <10){
+        time1Zero = 0
+      }
+     result = "" + time1Zero+parseInt(theTime1)+":"+result; 
+    }else{
+      result = "00"
+    } 
+    if(theTime2 > 0) { 
+     result = ""+parseInt(theTime2)+":"+result; 
+    }else{
+      result = "0"
+    } 
+    if(theTime3 > 0) { 
+     result = ""+parseInt(theTime3)+":"+result; 
+    }
+    return result; 
+   },
+  onLoad: function () {
+    var that = this;
+    var info = that.data.info;
+    this.data.inteval = setInterval(function(){
+      for(var i=0;i<info.length;i++){
+        info[i].second = info[i].second-1
+        var times = that.formatSeconds(info[i].second)
+        info[i].time = times;
+        that.setData({
+          info:info
+        })
+      }
+    },1000)
+  },
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
